4e63c920016800098562306c32529441517feb3a
Agnes Preethi <agnespreethi98@gmail.com>
2024-09-12 13:00:54 +0530

    Java Basic
    Java Basic

---
+++ DemoStaticVariable.java
@@ -0,0 +1,34 @@
+class Mobile {
+    String brand;
+    int price;
+    static String type;
+
+    public void show() {
+        System.out.println(brand + " : " + price + " : " + type);
+    }
+}
+
+public class DemoStaticVariable {
+    public static void main(String a[]) {
+        Mobile obj1 = new Mobile();
+        obj1.brand = "IPhone";
+        obj1.price = 17000;
+        // obj1.type = "Smart Phone";
+        Mobile.type = "Smart Phone";
+
+        Mobile obj2 = new Mobile();
+        obj2.brand = "Samsung";
+        obj2.price = 15000;
+        // obj2.type = "Smart Phone";
+        Mobile.type = "Smart Phone";
+
+        Mobile obj3 = new Mobile();
+        obj3.brand = "Nokia";
+        obj3.price = 5000;
+        Mobile.type = "Phone";
+
+        obj1.show();
+        obj2.show();
+        obj3.show();
+    }
+}
