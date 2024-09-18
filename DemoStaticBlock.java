1a4dce9028ba12016f6939d6cf17b0d4a8c452db
Agnes Preethi <agnespreethi98@gmail.com>
2024-09-18 10:56:00 +0530

    Java Basics
    Java Basics

---
+++ DemoStaticBlock.java
@@ -0,0 +1,35 @@
+class Mobile {
+    String brand;
+    int price;
+    static String type;
+
+    public Mobile(){
+        brand ="";
+        price = 20000;
+        System.out.println("------- Constructor Block ---------");
+    }
+
+    static{
+        type = "phone";
+        System.out.println("--------- In Static Block ----------");
+    }
+
+    public void show() {
+        System.out.println(brand + " : " + price + " : " + type);
+    }
+}
+
+public class DemoStaticBlock {
+    public static void main(String a[]) {
+
+        //Class.forName("Mobile");  // To load class without object
+        Mobile obj1 = new Mobile();
+        obj1.brand = "IPhone";
+        obj1.price = 17000;
+        Mobile.type = "Smart Phone";
+
+        Mobile obj2 = new Mobile();
+        
+    }
+}
+
