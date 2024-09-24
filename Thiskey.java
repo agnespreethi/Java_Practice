1081db8cbb59acd8edd811dd62caea506ba1000d
Agnes Preethi <agnespreethi98@gmail.com>
2024-09-24 12:14:49 +0530

    Updated
    Updated

--- Thiskey.java
+++ Thiskey.java
@@ -1,30 +1,33 @@
-class Human{
+class Human {
     private int age;
     private String name;
 
-    public int getAge(){
+    public int getAge() {
         return age;
     }
-    // Assigning to local varible it's self
-    // public void setAge(int age){
-    //     age = age;
-    // }
 
-    // Assigning with object
-    //  public void setAge(int age, Human obj){
-    //     Human obj1 = obj;
-    //      obj1.age = age;
-    //  }
-
-    // Using default object (this keyword)
+    /* Assigning to local varible it's self
     public void setAge(int age){
-             this.age = age;
+        age = age;
+    } */
+
+    /*  Assigning with object
+     public void setAge(int age, Human obj){
+        Human obj1 = obj;
+         obj1.age = age;
+     } */
+
+
+    // Using the 'this' keyword
+    public void setAge(int age) {
+        this.age = age;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
-    public void setName(String n){
+
+    public void setName(String n) {
         name = n;
     }
 }
@@ -33,9 +36,8 @@ public class Thiskey {
     public static void main(String[] args) {
         Human obj = new Human();
         obj.setAge(27);
-        //obj.setAge(27,obj);
+        // obj.setAge(27,obj);
         obj.setName("Monica");
         System.out.println(obj.getName() + " : " + obj.getAge());
     }
-
 }
