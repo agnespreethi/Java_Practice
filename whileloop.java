3e7f21053ee4a2bc2cc0f42c83420a04e8330bfb
Agnes Preethi <agnespreethi98@gmail.com>
2025-06-10 17:46:05 +0530

    push code

---
+++ whileloop.java
@@ -0,0 +1,10 @@
+public class whileloop {
+    public static void main(String[] args) {
+        int x = 10;
+        while (x<20) {
+            System.out.print("The value of x is :"  + x);
+            x++;
+            System.out.print("\n");
+        }
+    }
+}
