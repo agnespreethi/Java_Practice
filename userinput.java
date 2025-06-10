d9768636bf74253dc8c8ae759d37582641b5d928
Agnes Preethi <agnespreethi98@gmail.com>
2025-06-10 19:04:14 +0530

    push code

--- userinput.java
+++ userinput.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 public class userinput {
     public static void main(String[] args) {
         // Create scanner object
-        Scanner input = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         // output
         System.out.print("Enter a floating point value :");
 
         // wait for the user to enter something
-        double value = input.nextDouble();
+        double value = scanner.nextDouble();
 
         // tell the user what was entered
         System.out.print("You have entered :" + value);
