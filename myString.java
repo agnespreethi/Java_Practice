95875f153c8a2fd17bec4376df22a2ef959ff09d
Agnes Preethi <agnespreethi98@gmail.com>
2025-06-10 13:46:10 +0530

    edited

--- myString.java
+++ myString.java
@@ -1,17 +1,17 @@
 public class myString {
    public static void main(String[] args) {
 
-int myInt = 7;
-String text ="Java";
-String blank = " ";
-String name = "Preethi";
-String greeting = text + blank + name;
-System.out.println(greeting);
-System.out.println("Java" + " " + "Preethi");
-System.out.println("My Integer is : " + myInt);
+      int myInt = 7;
+      String text = "Java";
+      String blank = " ";
+      String name = "Preethi";
+      String greeting = text + blank + name;
+      System.out.println(greeting);
+      System.out.println("Java" + " " + "Preethi");
+      System.out.println("My Integer is : " + myInt);
 
-double myDouble = 5.5;
-System.out.println("My number is : " + myDouble + ".");
+      double myDouble = 5.5;
+      System.out.println("My number is : " + myDouble + ".");
 
-}
+   }
 }
