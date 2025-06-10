26486fe9eb6266c7d08e712c356c34ff7a3a0b5e
Agnes Preethi <agnespreethi98@gmail.com>
2025-06-10 19:26:17 +0530

    push code

---
+++ switchdemo.java
@@ -0,0 +1,24 @@
+public class switchdemo {
+    public static void main(String[] args) {
+        int week = 7;
+        switch (week) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break; 
+            case 4:
+                System.out.println("Thursday");
+                break; 
+            case 5:
+                System.out.println("Friday");
+                break;          
+            default:
+                System.out.println("Saturday");
+        }
+    }
+}
