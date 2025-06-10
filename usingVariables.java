aa065bc16bbd5532b90132f8eeaad78e5b404857
Agnes Preethi <agnespreethi98@gmail.com>
2025-06-10 11:19:08 +0530

    code push

---
+++ usingVariables.java
@@ -0,0 +1,22 @@
+public class usingVariables {
+     public static void main(String[] args) {
+
+        int myNumber = 88;
+        short myShort = 847;
+        long myLong = 9797L;
+        double myDouble = 7.3541;
+        float myFloat = 324.3f;
+        char myChar = 'y';
+        boolean myBoolean = false;
+        byte myByte = 124;
+           
+        System.out.println(myNumber);
+        System.out.println(myShort);
+        System.out.println(myLong);
+        System.out.println(myDouble);
+        System.out.println(myFloat);           
+        System.out.println(myChar);
+        System.out.println(myBoolean);
+        System.out.println(myByte);
+      }
+}
