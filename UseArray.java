4ad8fba8729dd7c814b041d054ab0822517df41b
Agnes Preethi <agnespreethi98@gmail.com>
2025-06-15 11:35:15 +0530

    code push

---
+++ UseArray.java
@@ -0,0 +1,11 @@
+
+public class UseArray {
+public static void main(String[] args) {
+	double[] Alist={1,2,3};
+	double Total=0;
+	for(int i=0; i<Alist.length; i++){
+		Total += Alist[i];
+    }
+    System.out.print("Total is: " + Total);
+}
+}
