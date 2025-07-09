07ae759529cb55cce98b3b1fc4c9bec818e6cb70
Agnes Preethi <77042507+agnespreethi@users.noreply.github.com>
2025-07-09 15:08:26 +0530

    Udemy codes

--- Cons.java
+++ Cons.java
@@ -10,6 +10,11 @@ class Huma {
         name = "Rinika";
     }
 
+    public Huma(int age, String name) {
+        this.name = name;
+        this.age = age;
+    }
+
     public int getAge() {
         return age;
     }
@@ -31,9 +36,9 @@ class Huma {
 public class Cons {
     public static void main(String[] args) {
         Huma obj = new Huma();
-        // Huma obj1 = new Huma();
+        Huma obj1 = new Huma(10,"Preethi");
         System.out.println(obj.getName() + " : " + obj.getAge());
-
+        System.out.println(obj.getName() + " : " + obj.getAge());
         // obj.setAge(30);
         // obj.setName("Stephen");
     }
