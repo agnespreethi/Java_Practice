2dc7d0dd75ad0e661fab4868f20b0ad9640df370
Agnes Preethi <77042507+agnespreethi@users.noreply.github.com>
2025-07-09 15:30:43 +0530

    Udemy code

--- InherCalc.java
+++ InherCalc.java
@@ -1,10 +1,12 @@
 public class InherCalc {
     public static void main(String[] args) {
-        Advcalc obj = new Advcalc();
+        VeryAdvCalc obj = new VeryAdvCalc();
+        //Advcalc obj = new Advcalc();
         int r1 = obj.add(2,3);
         int r2 = obj.sub(7,4);
         int r3 = obj.mul(5,3);
         int r4 = obj.div(6,2);
-        System.out.println(r1 + " " + r2 + " " + r3 + " " + r4);
+        double r5 = obj.power(5,2);
+        System.out.println(r1 + " " + r2 + " " + r3 + " " + r4 + " " + r5);
     }
 }
